413a6863d4cc0c9dd73ea8f75bd83ef817d5aad1
teolandon <kalfastheodoros@gmail.com>
2022-12-26 18:12:16 -0600

    workaround for cron using npm

--- cron/fetchStats.ts
+++ cron/fetchStats.ts
@@ -59,7 +59,7 @@ async function main() {
     console.log('Pending git changes... aborting deploy');
     return
   }
-  const { stdout: stdout2, stderr: stderr2 } = await execPromise(`npm run --prefix ${rootDir} deploy`);
+  const { stdout: stdout2, stderr: stderr2 } = await execPromise(`/Users/teolandon/.nvm/versions/node/v18.12.0/bin/npm run --prefix ${rootDir} deploy`);
   console.log(stdout2);
   if(stderr2) {
     console.error(stderr2);
