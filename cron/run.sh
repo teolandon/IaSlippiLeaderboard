05aea8d01c39cbb84561339ecf8bf681816744cb
teolandon <kalfastheodoros@gmail.com>
2022-12-29 05:49:38 +0200

    Remove log pipe

--- cron/run.sh
+++ cron/run.sh
@@ -2,4 +2,4 @@
 
 DIR_PATH=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd "$DIR_PATH/.." 
-node --loader ts-node/esm --no-warnings cron/fetchStats.ts 2>&1 | tee cron/logs/log.txt
+node --loader ts-node/esm --no-warnings cron/fetchStats.ts
