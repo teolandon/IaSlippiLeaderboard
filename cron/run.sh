31d597a0a083715c51d457a1d54c4dada5ff2173
Theo Kalfas <tkalfas@sourceallies.com>
2022-12-29 04:55:47 +0200

    Keep on reverting

--- cron/run.sh
+++ cron/run.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
 DIR_PATH=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-NODE_BIN=/Users/teolandon/.nvm/versions/node/v18.12.0/bin/node
 cd "$DIR_PATH/.." 
-$NODE_BIN --loader ts-node/esm --no-warnings cron/fetchStats.ts 2>&1 | tee cron/logs/log.txt
+node --loader ts-node/esm --no-warnings cron/fetchStats.ts 2>&1 | tee cron/logs/log.txt
