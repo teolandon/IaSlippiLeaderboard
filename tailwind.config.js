5ba3c02fdea508e7a11d82acf79628eb026ec9e2
Jacob Miller <158308043+Jamiller137@users.noreply.github.com>
2025-05-18 00:47:45 -0500

    Update tailwind.config.js for diggles

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'hero-pattern': "url('/diggles137.png')",
+      },
+    },
   },
   safelist: [
     'bg-gray-600'
